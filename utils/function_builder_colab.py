1d78b4b201d2682d341befa5657f035895568d46
Thomas Goter <tomgoter@gmail.com>
2019-11-10 08:03:51 -0500

    numpy import

--- utils/function_builder_colab.py
+++ utils/function_builder_colab.py
@@ -40,7 +40,7 @@ def construct_scalar_host_call(
         return tf.contrib.summary.all_summary_ops()
 
   global_step_tensor = tf.reshape(tf.train.get_or_create_global_step(), [1])
-  other_tensors = [tf.reshape(monitor_dict[key], [1]) for key in metric_names]
+  other_tensors = [tf.reshape(monitor_dict[key], [-1]) for key in metric_names]
 
   return host_call_fn, [global_step_tensor] + other_tensors
 
